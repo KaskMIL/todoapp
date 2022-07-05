e053a92a3eb062c4acda5bbe4ecde7b7a7f33749
toMilanesi <tomi.milanesi@gmail.com>
2022-07-05 15:48:19 -0300

    Refactor InpuTodo to Hook

--- src/InputTodo.js
+++ src/InputTodo.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class InputTodo extends Component {
-  state = {
-    title: '',
-  };
-  onChange = (e) => {
-    this.setState({
+const InputTodo = props => {
+  const [inputText, setInputText] = useState({
+    title: ''
+  });
+
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
       [e.target.name]: e.target.value,
     });
   };
-  handleSubmit = (e) => {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.title.trim()) {
-      this.props.addItemProps(this.state.title);
-      this.setState({
+    if (inputText.title.trim()) {
+      props.addItemProps(inputText.title);
+      setInputText({
         title: '',
       });
     } else {
       alert('Please write an Item');
     }
   };
-  render() {
-    return (
-      <form className="form-container" onSubmit={this.handleSubmit}>
-        <input
-          className="input-text"
-          type="text"
-          name="title"
-          placeholder="Add Todo"
-          value={this.state.title}
-          onChange={this.onChange}
-        />
-        <button className="input-submit">Submit</button>
-      </form>
+  
+  return (
+     <form className="form-container" onSubmit={handleSubmit}>
+       <input
+         className="input-text"
+         type="text"
+         name="title"
+         placeholder="Add Todo"
+         value={inputText.title}
+         onChange={onChange}
+       />
+       <button className="input-submit">Submit</button>
+     </form>
     );
   }
-}
+
 
 export default InputTodo;
